e250655f987b8f9da5d5fc77ff1201dcf05940e0
miracleonyenma <miracleficient@gmail.com>
2022-03-17 09:56:11 +0100

    fix*: add cloudinary config straight in main file

--- functions/miracleio-generate-preview.js
+++ functions/miracleio-generate-preview.js
@@ -3,7 +3,11 @@ const puppeteer = require("puppeteer-core");
 const fs = require("fs");
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config(require("../cloudinary.config"));
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const uploadScreenshot = (options, screenshot) => {
 	return new Promise((resolve, reject) => {
